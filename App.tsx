19f56565a6cded7a8f869c3d6cfb4f281a16ab74
alak <alak@estatelogs.com>
2021-01-05 18:19:54 +0200

    need to fix font-family

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native'
+
+import { Restaurant, OrderDelivery } from './screens'
+import Tabs from './navigation/tabs'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Home'}
+            >
+                <Stack.Screen name="Home" component={Tabs} />
+                <Stack.Screen name="Restaurant" component={Restaurant} />
+                <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ constants/maps.tsx
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = `AIzaSyC7VSCNVvqxfNhbsvCadl4FoxDyZ35c-wo`;
+
+export default GOOGLE_API_KEY
